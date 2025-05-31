2c9d4da0c549f7422437e2549866d328892b0728
Saffet <northlethe@gmail.com>
2025-05-31 18:51:18 +0300

    Admin panelde Bekleyen taleplerin görünmemesi sorunu giderildi.

--- client/src/pages/admin/AdminPanel.js
+++ client/src/pages/admin/AdminPanel.js
@@ -144,7 +144,7 @@ const AdminPanel = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 flex flex-col">
-      {/* Sol Menü */}
+      {/* Sol Menü */} 
       <AdminSidebar 
         activeTab={activeTab} 
         setActiveTab={setActiveTab}
